96aeb46b5bebe3c9dd1df8dfb1ef33286cf930e0
guillzera <guiguidefarias36@gmail.com>
2024-09-16 19:15:59 -0300

    basico de classes

---
+++ Classes/Models/Funcionario.cs
@@ -0,0 +1,8 @@
+﻿namespace Classes.Models
+{
+    internal class Funcionario
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,21 @@
+﻿using Classes.Models;
+using System.Globalization;
+
+Funcionario funcionario1 = new Funcionario();
+Funcionario funcionario2 = new Funcionario();
+
+Console.WriteLine("Dados do primeiro funcionário:");
+
+Console.Write("Nome: ");
+funcionario1.Nome = Console.ReadLine();
+Console.Write("Salario: ");
+funcionario1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Dados do segundo funcionário");
+Console.Write("Nome: ");
+funcionario2.Nome = Console.ReadLine();
+Console.Write("Salario: ");
+funcionario2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double salarioMedio = (funcionario1.Salario + funcionario2.Salario) / 2.0;
+Console.WriteLine("Salário médio: " + salarioMedio.ToString("F2", CultureInfo.InvariantCulture));
